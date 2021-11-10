ba28d3aadccf5b814b6809b5f88b716617edeadb
taehyung.kim <kimtaehyeong62@gmail.com>
2021-11-10 12:51:49 +0900

    test1
    test1

---
+++ mnist_sample_1.py
@@ -0,0 +1,91 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+
+args={}
+kwargs={}
+args['batch_size']=128
+args['test_batch_size']=64
+args['epochs']=100000  #The number of Epochs is the number of times you go through the full dataset. 
+args['lr']=0.01 #Learning rate is how fast it will decend. 
+args['momentum']=0.5 #SGD momentum (default: 0.5) Momentum is a moving average of our gradients (helps to keep direction).
+
+args['seed']=1 #random seed
+args['log_interval']=10
+args['cuda']=True
+
+
+#load the data
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args['batch_size'], shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args['test_batch_size'], shuffle=True, **kwargs)
+
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if args['cuda']:
+            data, target = data.cuda(), target.cuda()
+        #Variables in Pytorch are differenciable. 
+        data, target = Variable(data), Variable(target)
+        #This will zero out the gradients for this batch. 
+        optimizer.zero_grad()
+        output = model(data)
+        # Calculate the loss The negative log likelihood loss. It is useful to train a classification problem with C classes.
+        loss = F.nll_loss(output, target)
+        #dloss/dx for every Variable 
+        loss.backward()
+        #to do a one-step update on our parameter.
+        optimizer.step()
+        #Print out the loss periodically. 
+        if batch_idx % args['log_interval'] == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if args['cuda']:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+model = Net()
+if args['cuda']:
+    model.cuda()
+
+optimizer = optim.SGD(model.parameters(), lr=args['lr'], momentum=args['momentum'])
+
+for epoch in range(1, args['epochs'] + 1):
+    train(epoch)
+    test()
+
+
